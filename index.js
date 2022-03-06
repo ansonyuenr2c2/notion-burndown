e9420b82eb8cdcf618b2e1bdeb0a4279adc49926
ansonyuenr2c2 <100556578+ansonyuenr2c2@users.noreply.github.com>
2022-03-06 20:22:36 +0800

    fix: unified timezone in both github workflow and index.js; avoid runner congestion (#1)
    - changed timezone in index.js to Asia/Hong_Kong
    
    - set cron to an odd time to avoid runner congestion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client } = require("@notionhq/client");
-const moment = require("moment");
+const moment = require("moment-timezone");
+moment.tz.setDefault("Asia/Hong_Kong");
 const ChartJSImage = require("chart.js-image");
 const log = require("loglevel");
 const fs = require("fs");
